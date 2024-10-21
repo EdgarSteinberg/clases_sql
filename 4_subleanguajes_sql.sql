cabe8cd66f8a40660827440e8898bb1bf23f2a41
Edgar Steinberg <132410006+EdgarSteinberg@users.noreply.github.com>
2024-10-21 14:48:48 -0300

    add join

--- 4_subleanguajes_sql.sql
+++ 4_subleanguajes_sql.sql
@@ -57,5 +57,74 @@ FROM
 WHERE 
 	name NOT LIKE 'F%'
 	AND id_level IN (11,13,1)
-ORDER BY id_level
+    AND id_class != 195
+    AND id_game BETWEEN 20 AND 50
+ORDER BY id_level, id_class
 ;
+
+SELECT
+	*
+FROM 
+	coderhouse_gamers.system_user;
+    
+-- CONCAT 
+-- LOWER 
+-- ADD COLS
+
+SELECT
+	CONCAT(last_name, ', ', first_name) AS Nombre_Completo
+FROM
+	coderhouse_gamers.system_user;
+    
+SELECT
+	LOWER(CONCAT(last_name, ', ', first_name)) AS Nombre_Completo,
+    CONCAT('Bienvenido: ', email) AS mensaje_bienvenida,
+    'Password: changeme123' AS password_user
+FROM
+	coderhouse_gamers.system_user;
+    
+-- Funciones de Agregado
+-- GRUP BY -- HAVING
+SELECT
+	id_user_type,
+    COUNT(DISTINCT id_system_user) AS Total_De_Usuarios
+FROM coderhouse_gamers.system_user
+GROUP BY id_user_type
+HAVING Total_De_Usuarios > 5
+;
+
+-- Que juego tuvo mas cantidad de comentarios
+SELECT
+	id_game,
+    COUNT(commentary) AS Total_De_Comentarios_Por_Juego
+FROM	
+	coderhouse_gamers.commentary
+GROUP BY id_game
+ORDER BY  Total_De_Comentarios_Por_Juego DESC
+LIMIT 1
+;
+
+
+-- JOIN
+-- UN JOIN POR DEFAULT SIEMPRE ES INNERJOIN
+SELECT 
+    *
+FROM
+    coderhouse_gamers.game;
+SELECT 
+    *
+FROM
+    coderhouse_gamers.commentary;
+
+SELECT
+	c.id_game,
+    COUNT(c.commentary) AS total_de_comentarios,
+    g.*
+FROM
+	coderhouse_gamers.commentary AS c
+JOIN coderhouse_gamers.game as g
+	on c.id_game = g.id_game
+GROUP BY c.id_game
+ORDER BY total_de_comentarios DESC
+LIMIT 10;
+
